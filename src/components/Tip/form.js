edbca962d401cbb35aed6347c1e3a8b8ead49576
Tatiana Ivanova <ivanova.tatiana.w@gmail.com>
2021-01-26 13:36:17 +0300

    add form for tip

--- src/components/Tip/form.js
+++ src/components/Tip/form.js
@@ -1,5 +1,29 @@
 import React from "react";
+import { Form, Button } from "react-bootstrap";
+import { useState } from "react";
 
 export default function TipForm() {
-  return <div>and I am a Form!</div>;
+  const [text, set_text] = useState("");
+  console.log(`bla bla bla`, text);
+  return (
+    <div style={{ margin: "20px" }}>
+      <Form>
+        <Form.Group controlId="exampleForm.ControlTextarea1">
+          <Form.Label>
+            <h6>
+              <b>Add your tip!</b>
+            </h6>
+          </Form.Label>
+          <Form.Control
+            onChange={(e) => {
+              set_text(e.target.value);
+            }}
+            as="textarea"
+            rows={3}
+          />
+        </Form.Group>
+        <Button variant="info">Add tip</Button>
+      </Form>
+    </div>
+  );
 }
