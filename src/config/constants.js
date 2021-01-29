2db2a9d0e5969c6dd56dae377305d612bb8df08b
Guido Westra <westra.guido@gmail.com>
2021-01-29 10:23:42 +0100

    typo

--- src/config/constants.js
+++ src/config/constants.js
@@ -1,8 +1,7 @@
 export const apiUrl =
   process.env.NODE_ENV === "production"
-    ? "https://travel--tips.herokuapp.com/"
+    ? "https://travel--tips.herokuapp.com"
     : "http://localhost:4000";
 
 export const DEFAULT_MESSAGE_TIMEOUT = 3000;
-export const CLOUDINARY_URL =
-  "https://api.cloudinary.com/v1_1/dxtq8ajzg/upload";
+export const CLOUDINARY_URL = "https://api.cloudinary.com/v1_1/dxtq8ajzg/upload";
